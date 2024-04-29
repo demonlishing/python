ebd0af39e33ac1fb1f7703125c72085dc7cdbb85
Murilo <129951122+demonlishing@users.noreply.github.com>
2024-04-29 12:33:24 -0300

    Add files via upload

---
+++ cadastro.py
@@ -0,0 +1,75 @@
+
+print("--CADASTRO DA PRIMEIRA PESSOA--")
+cadastro1 = input('Digite seu nome: ')
+idade1 = int(input('Digite sua idade: ') )
+sexo1 = input('Digite seu sexo: ')
+
+if(idade1 <18):
+    print("A primeira pessoa é menor de idade")
+else:
+    print("A primeira pessoa é maior de idade")
+
+
+print("--CADASTRO DA SEGUNDA PESSOA--")
+
+cadastro2 = input('Digite seu nome: ')
+idade2 = int(input('Digite sua idade: ') )
+sexo2 = input('Digite seu sexo: ')
+
+if(idade2 <18):
+    print("A segunda pessoa é menor de idade")
+else:
+    print("A segunda pessoa é maior de idade")
+
+
+print("--CADASTRO DA TERCEIRA PESSOA--")
+
+cadastro3 = input('Digite seu nome: ')
+idade3 = int(input('Digite sua idade: ') )
+sexo3 = input('Digite seu sexo: ')
+
+
+if(idade3 <18):
+    print("A terceira pessoa é menor de idade")
+else:
+    print("A terceira pessoa é maior de idade")
+
+
+print("------CADASTRO------")
+print("--PRIMEIRA PESSOA--")
+print ("Nome:", cadastro1)
+print ("idade:", idade1)
+print ("sexo:", sexo1)
+if(idade1 <18):
+    print("MENOR DE IDADE")
+else:
+    print("MAIOR DE IDADE")
+
+
+
+
+
+
+print("================================")
+print("--SEGUNDA PESSOA--")
+print ("Nome:", cadastro2)
+print ("idade:", idade2)
+print ("sexo:", sexo2)
+if(idade2 <18):
+    print("MENOR DE IDADE")
+else:
+    print("MAIOR DE IDADE")
+
+
+
+
+
+print("================================")
+print("--TERCEIRA PESSOA--")
+print ("Nome:", cadastro3)
+print ("idade:", idade3)
+print ("sexo:", sexo3)
+if(idade3 <18):
+    print("MENOR DE IDADE")
+else:
+    print("MAIOR DE IDADE")
