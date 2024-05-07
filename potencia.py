bf1973bf50f12d9a903860136be25ae4e5f6224d
Murilo <129951122+demonlishing@users.noreply.github.com>
2024-05-07 10:23:43 -0300

    Create potencia.py

---
+++ potencia.py
@@ -0,0 +1,7 @@
+base = int(input("Digite um número para a base da potencia:"))
+expoente = int(input("Digite um número para o expoente da potencia:"))
+
+
+
+resultado=base ** expoente
+print("O numero",base,"com expoente", expoente,"é:", resultado)
