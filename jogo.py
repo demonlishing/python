e55b1d1cde00ea272afd999112fca98414f7cba1
Murilo <129951122+demonlishing@users.noreply.github.com>
2024-05-13 08:56:29 -0300

    Update jogo.py
    adicionando as tentativas, erros e acertos.

--- jogo.py
+++ jogo.py
@@ -1,20 +1,26 @@
 import random
-continuar = True
-continuar = "s"
-while continuar == "s":
+ 
+continuar = "S"
+pERRO = 0
+pCERTO = 0
+
+while continuar.upper() == "S":
     ns = random.randint(1,10)
-
-    T = 5
-    while( T > 0 ):
-        print("Você tem", T, "tentativas")
+    T = 3
+    while(T > 0):
+        print("Você tem", T, " Tentativas")
         T = T - 1
-        chute = int(input("Digite um numero entre 0 e 10:"))
-
-        if( ns == chute):
+        nc = int(input("Digite um número entre 1 e 10:"))
+        if (ns == nc):
             print("Você acertou.")
             T = 0
+            pCERTO = pCERTO + 1
         else:
             print("Você errou.")
-             
-    continuar = input("Deseja continuar? Se Sim, digite (s)")
+            pERRO = pERRO + 1
+        print("Número de acertos:", pCERTO)
+        print("Número de errros:", pERRO)
 
+
+
+    continuar = input("Deseja continuar? (S)im:")
