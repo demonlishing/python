851d2f41a613f37906721929c2883d0f0857fc25
Murilo <129951122+demonlishing@users.noreply.github.com>
2024-05-21 10:55:48 -0300

    Create number.py

---
+++ number.py
@@ -0,0 +1,18 @@
+num1 = 5
+num2 = 5
+print("Resultado:", num1 + num2)
+
+n3 = "8"
+n4 = "5"
+print("Resultado:", n3 + n4)
+n5 = "8 "
+n6 = "5 x"
+
+print("Resultado:", n5 + n6)
+
+n8 = input("Digite um valor: ")
+print("Resultado:", n8 + "5")
+
+
+n9 = int (input("Digite um valor:"))
+print("Resultado:", n9 + int("5"))
